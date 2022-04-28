78550136cec792abb15101b4e1be7277b25ae03c
Andy Nilson <andy@andynilson.com>
2022-04-28 08:19:04 -0700

    updated filenames

--- sampleMain.py
+++ sampleMain.py
@@ -3,14 +3,25 @@ import mosspy
 userid = 505447473
 moss = mosspy.Moss(userid, "python")
 
+# File submissions for sum of left leaves problem
 
-# Files
-moss.addFile("sumOfLeftLeavesWhileLoopSolutionBase.py")
-moss.addFile("CarbonCopyOfsumOfLeftLeavesWhileLoopSolution.py")
-moss.addFile("sumOfLeftLeavesWhileLoopSolutionVariabeDeclarationOrderChangedMultipleComments.py")
-moss.addFile("sumOfLeftLeavesWhileLoopSolutionVariabeDeclarationOrderChangedMultipleComments.py")
-moss.addFile("sumOfLeftLeavesWhileLoopSolutionBaseWithCommentsStrategicallyPlacedToHidePlagiarism.py")
-moss.addFile("SumOfLeftLeavesPointToPointSolutionBase.py")
+# base while loop solution
+moss.addFile("baseWhileLoopSolution.py")
+
+# simple copy paste of base solution
+moss.addFile("copyOfWhileLoopSolution.py")
+
+# variable declaration order changed and a few single-line comments added
+moss.addFile("solutionWithMinorChanges.py")
+
+# variable declaration order changed and a few multi-line comments added
+moss.addFile("solutionWithBiggerChanges.py")
+
+# variable declaration order changed and comments strategically added to hide plagiarism
+moss.addFile("solutionWithStrategicChanges.py")
+
+# basic point to point solution - completely different to while loop
+moss.addFile("pointToPointSolution.py")
 
 # progress function optional, run on every file uploaded
 # result is submission URL
@@ -19,6 +30,3 @@ print()
 
 # print report url
 print("Report Url: " + url)
-
-
-
